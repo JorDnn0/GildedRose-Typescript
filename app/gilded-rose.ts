cf5d7d5ffd1095bdb7e64a0a4e0cd443de745d71
JorDnn0 <90001659+JorDnn0@users.noreply.github.com>
2023-06-29 13:58:50 +0100

    Refactoring

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -33,7 +33,9 @@ export class GildedRose {
 
             //Backstage passes case
             }else if(this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert'){
-                if (this.items[i].quality < 50) {
+                if (this.items[i].sellIn < 0) {
+                    this.items[i].quality = 0
+                } else if (this.items[i].quality < 50) {
                     if (this.items[i].sellIn <= 5) {
                         this.items[i].quality = this.items[i].quality + 3
                     }
@@ -44,7 +46,7 @@ export class GildedRose {
                     }
                 }
 
-            //
+            //Other item names
             }else{
                 if (this.items[i].quality > 0) {
                     this.items[i].quality = this.items[i].quality - 1
@@ -52,14 +54,13 @@ export class GildedRose {
             }
 
             this.items[i].sellIn = this.items[i].sellIn - 1;
+
             if (this.items[i].sellIn < 0) {
                 if (this.items[i].name != 'Aged Brie') {
                     if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
                         if (this.items[i].quality > 0) {
                             this.items[i].quality = this.items[i].quality - 1
                         }
-                    } else {
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality
                     }
                 } else {
                     if (this.items[i].quality < 50) {
