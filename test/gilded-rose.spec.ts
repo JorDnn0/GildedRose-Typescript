5cc3fc8ab88f599718939e3d66d8bfe2a3b11c91
JorDnn0 <90001659+JorDnn0@users.noreply.github.com>
2023-06-29 11:53:40 +0100

    Sulfuras quality test

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -3,10 +3,10 @@ import { Item, GildedRose } from '../app/gilded-rose';
 
 describe('Gilded Rose', function () {
 
-    it('should foo', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
+    it('Sulfuras quality 80', function() {
+        const gildedRose = new GildedRose([ new Item('Sulfuras, Hand of Ragnaros', 0, 80) ]);
         const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
+        expect(items[0].quality).to.equal(80);
     });
 
 });
